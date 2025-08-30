8a2ca443a7200a89cf2dd84725a96ac862723001
Shiba Parvin <shiba9118@gmail.com>
2025-08-30 16:12:45 +0530

    Add files via upload

---
+++ practical15.php
@@ -0,0 +1,20 @@
+<?php
+
+$color = "red"; 
+?>
+
+
+<html>
+<head>
+   
+    <title>Simple PHP with CSS</title>
+</head>
+<body>
+
+   
+    <p style="color: <?= $color ?>;">
+        This is a simple paragraph with the color set using PHP
+    </p>
+
+</body>
+</html>

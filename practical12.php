cd10779f5d040bcea862ccd9a9780a2e9a26b06a
Shiba Parvin <shiba9118@gmail.com>
2025-08-30 16:03:41 +0530

    Update practical12.php

--- practical12.php
+++ practical12.php
@@ -1,5 +1,21 @@
 <?php
-$num1 = 15;
-$num2 = 85;
-$sum = $num1 + $num2;
+
+$number1 = 8;
+$number2 = 12;
+
+$sum = $number1 + $number2;
 ?>
+
+
+<html>
+<head>
+    <title>Sum of Two Numbers</title>
+</head>
+<body>
+
+    <h1>Number 1: <?= $number1 ?></h1>
+    <h1>Number 2: <?= $number2 ?></h1>
+    <h2>The sum of the two numbers is: <?= $sum ?></h2>
+
+</body>
+</html>

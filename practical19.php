1d55c234c28e7d74b41efde78ba2f79307c2aa17
Shiba Parvin <shiba9118@gmail.com>
2025-08-30 18:23:58 +0530

    Add files via upload

---
+++ practical19.php
@@ -0,0 +1,89 @@
+<?php
+$name = "XYZ";
+
+$sub1 = 40;
+$sub2 = 30;
+$sub3 = 45;
+$sub4 = 48;
+$sub5 = 38;
+
+?>
+
+<html>
+<head>
+    <title>Marksheet</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 8px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+
+
+<table>
+   
+    <tr>
+        <td>Name : XYZ</td>
+        
+    </tr>
+    <tr>
+        <td>Subject Name</td>
+        <td>Max Marks</td>
+        <td>Obtained Marks</td>
+    </tr>
+    <tr>
+        <td>Sub1</td>
+        <td>50</td>
+        <td>40</td>
+    </tr>
+    
+    <tr>
+        <td>Sub2</td>
+        <td>50</td>
+        <td>30</td>
+    </tr>
+    <tr>
+        <td>Sub3</td>
+        <td>50</td>
+        <td>45</td>
+    </tr>
+
+    <tr>
+        <td>Sub4</td>
+        <td>50</td>
+        <td>48</td>
+    </tr>
+
+    <tr>
+        <td>Sub5</td>
+        <td>50</td>
+        <td>34</td>
+    </tr>
+    <tr>
+        <td>Total</td>
+        <td>250</td>
+        <td>197</td>
+    </tr>
+
+    
+    <tr>
+        <td>Percentage</td>
+        <td>78.8</td>
+        
+    </tr>
+
+
+    <tr>
+        <td>Result</td>
+        <td>Passed</td>
+        <td>With Distinction</td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ practical20.php
@@ -0,0 +1,16 @@
+<?php
+
+$array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+echo $array[0] . "<br>";
+echo $array[1] . "<br>";
+echo $array[2] . "<br>";
+echo $array[3] . "<br>";
+echo $array[4] . "<br>";
+echo $array[5] . "<br>";
+echo $array[6] . "<br>";
+echo $array[7] . "<br>";
+echo $array[8] . "<br>";
+echo $array[9] . "<br>";
+?>

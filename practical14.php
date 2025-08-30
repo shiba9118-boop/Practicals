fef29fcae36c683c5ea5934b287d283c65398125
Shiba Parvin <shiba9118@gmail.com>
2025-08-30 16:08:55 +0530

    Create practical14.php

---
+++ practical14.php
@@ -0,0 +1,21 @@
+<?php
+
+$number1 = 5;
+$number2 = 10;
+$number3 = 15;
+?>
+
+
+<html>
+<head>
+    <title>Sum of Three Numbers</title>
+</head>
+<body>
+
+    <h1>Number 1: <?= $number1 ?></h1>
+    <h1>Number 2: <?= $number2 ?></h1>
+    <h1>Number 3: <?= $number3 ?></h1>
+    <h2>The sum of the three numbers is: <?= $number1 + $number2 + $number3 ?></h2>
+
+</body>
+</html>
